7ac0858987ee2500f27d7f58828e28de0f8717fc
Varadraj-Kulkarni <varadrajkulkarni3@gmail.com>
2025-11-20 15:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def home():
     return render_template('index.html', earthquakes=earthquakes)
     
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5000)
